72207658541eb89d41c0ea69afe218931f1bfdad
sibvan <sibvan@icloud.com>
2025-11-22 14:04:47 +0400

    clear deploy branch

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { renderStack } from "./blocks/stack/stack";
-import { renderProjects } from "./blocks/projects/projects";
-import { renderAge, renderHeroData } from "./blocks/hero/hero";
-
-renderProjects();
-renderStack();
-renderAge();
-renderHeroData();
