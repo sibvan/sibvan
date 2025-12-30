96dbbee390f8d844b4601b0192d6f81740f7bbff
sibvan <sibvan@icloud.com>
2025-12-30 18:46:52 +0400

    add props for card component

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -1,13 +1,15 @@
 import { ReactNode } from "react";
+import clsx from "clsx";
 
 type CardProps = {
   children: ReactNode;
+  className?: string;
 };
 
 const Card = (props: CardProps) => {
-  const { children } = props;
+  const { children, className } = props;
 
-  return <div>{children}</div>;
+  return <div className={clsx(className, "rounded-[20px]")}>{children}</div>;
 };
 
 export default Card;
