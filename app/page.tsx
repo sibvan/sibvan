6626a30aad0457095d2f9888d0efb267ee9296e9
sibvan <sibvan@icloud.com>
2025-12-30 14:48:00 +0400

    add exp component to landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import { cms } from "@/cms";
 import { ExpItem } from "@/cms";
 
+import Experience from "@/components/sections/Experience";
+
 export default async function Home() {
   const experience = await cms.find({ type: ExpItem });
-  return <div>123</div>;
+  return (
+    <div className="">
+      <Experience expList={experience} />
+    </div>
+  );
 }
