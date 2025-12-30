88117289722f4cd65025080c81b277cd94685fdf
sibvan <sibvan@icloud.com>
2025-12-30 12:45:24 +0400

    add cms schema for experience

--- cms.ts
+++ cms.ts
@@ -1,47 +1,61 @@
-import {Config, Field} from 'alinea'
-import {createCMS} from 'alinea/next'
+import { Config, Field } from "alinea";
+import { createCMS } from "alinea/next";
 
 // Create types for your CMS schema
-const Page = Config.type('Page', {
+
+const ExpItem = Config.type("Место работы", {
   fields: {
-    title: Field.text('Title'),
-    path: Field.path('Path')
-  }
-})
+    title: Field.text("Должность"),
+    company: Field.object("Компания", {
+      fields: {
+        title: Field.text("Название"),
+        url: Field.url("Сайт"),
+      },
+    }),
+    period: Field.text("Период"),
+    responsibilities: Field.list("Обязанности", {
+      schema: {
+        Responsibility: Config.type("Обязанность", {
+          fields: {
+            description: Field.text("Описание"),
+          },
+        }),
+      },
+    }),
+  },
+});
 
 export const cms = createCMS({
   // List out available types in your schema
-  schema: {
-    Page
-  },
+  schema: { ExpItem },
 
   // Define the content structure of your CMS
   workspaces: {
-    main: Config.workspace('Example', {
-      source: 'content',
-      mediaDir: 'public',
+    main: Config.workspace("sibvan.dev", {
+      source: "content",
+      mediaDir: "public",
       roots: {
-        pages: Config.root('Example site', {
-          contains: ['Page']
+        pages: Config.root("Опыт", {
+          contains: ["ExpItem"],
         }),
-        media: Config.media()
-      }
-    })
+        media: Config.media(),
+      },
+    }),
   },
 
   baseUrl: {
     // Point to your local website
-    development: 'http://localhost:3000',
+    development: "http://localhost:3000",
     // The production URL of your website
-    production: 'https://example.com'
+    production: "https://sibvan.dev",
   },
 
   // Enable live previews after adding <cms.previews widget /> to your layout
   // preview: true,
 
   // The handler route URL
-  handlerUrl: '/api/cms',
+  handlerUrl: "/api/cms",
 
   // The admin dashboard will be bundled in this static file
-  dashboardFile: 'admin.html'
-})
+  dashboardFile: "admin.html",
+});
