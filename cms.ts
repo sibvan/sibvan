50a726d99a3784e1bbdd9faa99ab00e07448e24e
sibvan <sibvan@icloud.com>
2026-01-05 14:04:58 +0400

    upgrade cms schema

--- cms.ts
+++ cms.ts
@@ -1,15 +1,31 @@
 import { Config, Field } from "alinea";
 import { createCMS } from "alinea/next";
+import { FaGitAlt, FaTimeline, FaToolbox } from "react-icons/fa6";
 
-// Create types for your CMS schema
+export const Stack = Config.type("Технология", {
+  fields: {
+    logo: Field.image("Логотип"),
+    title: Field.text("Название"),
+  },
+});
 
-export const ExpItem = Config.type("Место работы", {
+export const Project = Config.type("Проект", {
+  fields: {
+    url: Field.url("Ссылка"),
+    video: Field.file("Видео"),
+    title: Field.text("Название"),
+    description: Field.text("Описание"),
+  },
+});
+
+export const Job = Config.type("Место работы", {
   fields: {
     title: Field.text("Должность"),
     company: Field.object("Компания", {
       fields: {
         title: Field.text("Название"),
         url: Field.url("Сайт"),
+        color: Field.text("Цвет"),
       },
     }),
     period: Field.text("Период"),
@@ -27,16 +43,25 @@ export const ExpItem = Config.type("Место работы", {
 
 export const cms = createCMS({
   // List out available types in your schema
-  schema: { ExpItem },
+  schema: { Project, Job, Stack },
 
   // Define the content structure of your CMS
   workspaces: {
     main: Config.workspace("sibvan.dev", {
       source: "content",
-      mediaDir: "public",
+      mediaDir: "public/cms",
       roots: {
-        pages: Config.root("Опыт", {
-          contains: ["ExpItem"],
+        jobs: Config.root("Опыт", {
+          contains: ["Job"],
+          icon: FaTimeline,
+        }),
+        projects: Config.root("Проекты", {
+          contains: ["Project"],
+          icon: FaGitAlt,
+        }),
+        stacks: Config.root("Стек", {
+          contains: ["Stack"],
+          icon: FaToolbox,
         }),
         media: Config.media(),
       },
