cbbf61876b28807e5ddae38c82ddccb6d0bf5477
sibvan <sibvan@icloud.com>
2026-01-05 14:03:44 +0400

    add projects section

---
+++ components/sections/ProjectsSection.tsx
@@ -0,0 +1,77 @@
+import Card from "../ui/Card";
+import Button from "../ui/Button";
+import clsx from "clsx";
+
+type ProjectsProps = {
+  projectsList: {
+    _id: string;
+    title: string;
+    description: string;
+    url: {
+      _target: string;
+      _url: string;
+    };
+    video: {
+      href: string;
+    };
+  }[];
+};
+
+const ProjectsSection = (props: ProjectsProps) => {
+  const { projectsList } = props;
+  console.log(projectsList);
+  
+  return (
+    <section className="cards-gap grid md:grid-cols-2 xl:grid-cols-3">
+      <a
+        href="https://github.com/sibvan"
+        target="_blank"
+        className="group xl:-order-2"
+      >
+        <Card className="card-default flex aspect-square flex-col justify-between transition-shadow duration-300 ease-in-out group-hover:inset-shadow-[0px_0px_0px_1000px] group-hover:inset-shadow-black group-hover:outline group-hover:-outline-offset-1 group-hover:outline-white/10">
+          <h2 className="text-title">Проекты</h2>
+          <div className="flex items-end justify-between">
+            <p className="text-24 opacity-80">
+              Перейти
+              <br />
+              на GitHub
+            </p>
+            <Button />
+          </div>
+        </Card>
+      </a>
+      {projectsList.map((item, id) => {
+        return (
+          <a
+            className={clsx(
+              id === 1 && "md:col-span-2 md:row-span-2 xl:-order-1",
+              "group",
+            )}
+            href={item.url._url}
+            target={item.url._target}
+            key={item._id}
+          >
+            <Card className="card-default flex aspect-square flex-col gap-6 transition-shadow duration-300 ease-in-out group-hover:inset-shadow-[0px_0px_0px_1000px] group-hover:inset-shadow-black group-hover:outline group-hover:-outline-offset-1 group-hover:outline-white/10">
+              <p className="text-16 card-url text-right">{item.url._url}</p>
+              <div className="bg-grey-100 flex-1 rounded-[10px] overflow-hidden">
+                <video className="object-cover w-full h-full" muted autoPlay loop>
+                  <source src={`/cms${item.video.href}`} type="video/webm" />
+                </video>
+              </div>
+
+              <div className="flex items-end justify-between">
+                <div className="flex-1">
+                  <p className="text-24 pb-2 opacity-80">{item.title}</p>
+                  <p className="text-16 opacity-50">{item.description}</p>
+                </div>
+                <Button />
+              </div>
+            </Card>
+          </a>
+        );
+      })}
+    </section>
+  );
+};
+
+export default ProjectsSection;
