832731c6eddc55b4dbade068d0b20ef68562f94f
sibvan <sibvan@icloud.com>
2026-01-05 16:04:09 +0400

    add pulse animation to project card

--- components/sections/ProjectsSection.tsx
+++ components/sections/ProjectsSection.tsx
@@ -20,7 +20,7 @@ type ProjectsProps = {
 const ProjectsSection = (props: ProjectsProps) => {
   const { projectsList } = props;
   console.log(projectsList);
-  
+
   return (
     <section className="cards-gap grid md:grid-cols-2 xl:grid-cols-3">
       <a
@@ -53,8 +53,14 @@ const ProjectsSection = (props: ProjectsProps) => {
           >
             <Card className="card-default flex aspect-square flex-col gap-6 transition-shadow duration-300 ease-in-out group-hover:inset-shadow-[0px_0px_0px_1000px] group-hover:inset-shadow-black group-hover:outline group-hover:-outline-offset-1 group-hover:outline-white/10">
               <p className="text-16 card-url text-right">{item.url._url}</p>
-              <div className="bg-grey-100 flex-1 rounded-[10px] overflow-hidden">
-                <video className="object-cover w-full h-full" muted autoPlay loop>
+              <div className="relative flex-1 overflow-hidden rounded-[10px]">
+                <div className="bg-grey-100 absolute h-full w-full animate-pulse"></div>
+                <video
+                  className="relative z-1 h-full w-full object-cover"
+                  muted
+                  autoPlay
+                  loop
+                >
                   <source src={`/cms${item.video.href}`} type="video/mp4" />
                 </video>
               </div>
